5646c9ac01c05558997799ef41bf67cf39bee539
rootpigx <rootpigx@gmail.com>
2018-11-12 14:49:51 +0800

    added bulma components & sidebar component

---
+++ src/components/Sidebar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class Sidebar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>
+        <h1>商业地产运营监控平台</h1>
+        <h2>华润万象城</h2>
+        <ul>
+          <li />
+          <li />
+          <li />
+          <li />
+          <li />
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Sidebar;
