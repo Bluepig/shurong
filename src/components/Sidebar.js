1f64457da560cd7985d665035431d20612277f33
rootpigx <rootpigx@gmail.com>
2018-11-12 18:05:48 +0800

    stying sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,29 +1,54 @@
 import React from 'react';
-import '../styles/style.sass';
-import { Columns } from 'react-bulma-components';
+import '../styles/sidebar.scss';
+
 class Sidebar extends React.Component {
   constructor(props) {
     super(props);
-
     this.state = {};
   }
   render() {
     return (
-      <div className="test">
-        <h1>商业地产运营监控平台</h1>
-        <h2>华润万象城</h2>
-        <Columns>
-          <Columns.Column>First Column</Columns.Column>
-          <Columns.Column>Second Column</Columns.Column>
-          <Columns.Column>Third Column</Columns.Column>
-          <Columns.Column>Fourth Column</Columns.Column>
-        </Columns>
+      <div className='sidebar'>
+        <h1 className='sidebar-first-header'>商业地产运营监控</h1>
+        <h2 className='sidebar-second-header'>长宁八八中心</h2>
+
         <ul>
-          <li />
-          <li />
-          <li />
-          <li />
-          <li />
+          <li>
+            <a href='#'>多项目资产管理</a>
+          </li>
+          <li>
+            <a href='#'>单项目资产管理</a>
+          </li>
+          <li>
+            <a href='#'>项目监测</a>
+          </li>
+          <li>
+            <a href='#'>项目详情</a>
+          </li>
+          <li>
+            <a href='#'>业态详情</a>
+          </li>
+          <li>
+            <a href='#'>店铺详情</a>
+          </li>
+          <li>
+            <a href='#'>异常列表</a>
+          </li>
+          <li>
+            <a href='#'>停车场管理</a>
+          </li>
+        </ul>
+
+        <ul>
+          <li>
+            <a href='#'>系统设置</a>
+          </li>
+          <li>
+            <a href='#'>数据录入</a>
+          </li>
+          <li>
+            <a href='#'>登出当前帐号</a>
+          </li>
         </ul>
       </div>
     );
