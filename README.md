56aa2f85d990cc3a34993fe681cb903a750c8e66
rootpigx <rootpigx@gmail.com>
2018-11-12 15:27:45 +0800

    updated markdown notes

--- README.md
+++ README.md
@@ -1,10 +1,24 @@
 # 11-8 Note:
+
 - Server DB initial setting
+
   - local db server running on `localhost:27017`
   - default db name: `myDB`
 
 - production API server (production back-end, don't confuse with react-webpack server for development)
+
   - listen to local 5000, start by `yarn start-server`
 
 - **Add Packages**
   - mongodb & d3
+
+# 11-12 Note:
+
+- Added loader for sasa functionalities
+  Adder UI framework BULMA
+
+- TO DO:
+  Styling sidebar
+
+- **Add Packages**
+  - react-bulma-components & node-sass
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../styles/style.sass';
+import { Columns } from 'react-bulma-components';
 class Sidebar extends React.Component {
   constructor(props) {
     super(props);
@@ -11,6 +12,12 @@ class Sidebar extends React.Component {
       <div className="test">
         <h1>商业地产运营监控平台</h1>
         <h2>华润万象城</h2>
+        <Columns>
+          <Columns.Column>First Column</Columns.Column>
+          <Columns.Column>Second Column</Columns.Column>
+          <Columns.Column>Third Column</Columns.Column>
+          <Columns.Column>Fourth Column</Columns.Column>
+        </Columns>
         <ul>
           <li />
           <li />
