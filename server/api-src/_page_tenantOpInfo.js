0c79291de89dea54c0c3b5d641974a4808c88fc0
Bingxu Gao <bingxug@gmail.com>
2018-11-21 15:43:36 +0800

    allow full shopName and full bizType query

--- server/api-src/_page_tenantOpInfo.js
+++ server/api-src/_page_tenantOpInfo.js
@@ -15,16 +15,6 @@ function getTenantOperationData(req, res, next) {
   let data = TenantOperationData.slice();
   let bizData = BizTypeOperationData.slice();
 
-  if (!(year || month || week)) {
-    // 全时段
-    if (shopName) {
-      res.status(200).json(data);
-    } else if (bizType) {
-      res.status(200).json(bizData);
-    }
-    return;
-  }
-
   if (year) data = data.filter(o => o.year === year);
   if (month) data = data.filter(o => o.month === month);
   if (week) data = data.filter(o => o.week === week);
@@ -35,11 +25,12 @@ function getTenantOperationData(req, res, next) {
 
   if (!(shopName || bizType)) {
     res.status(400).json("Specify shop name or biz type");
-  }
+    return
+  } else
 
   // shop data
-  data = data.filter(o => (shopName ? o.name === shopName : true));
-  bizData = bizData.filter(o => (bizType ? o.bizType === bizType : true));
+  data = data.filter(o => (shopName && shopName !== 'full' ? o.name === shopName : true));
+  bizData = bizData.filter(o => (bizType && bizType !== 'full' ? o.bizType === bizType : true));
 
   if (shopName) {
     res.status(200).json(data);
